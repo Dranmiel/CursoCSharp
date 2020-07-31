564bf6a55fad0035c2c3e2584b0a75a65ac040fc
Caio Queiroz <caioqueirozn@gmail.com>
2020-07-31 16:10:32 -0300

    Problema de lista
    solução de um problema envolvendo lista

---
+++ DecimoSegundo/DecimoSegundo/Funcionario.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+
+namespace DecimoSegundo {
+    class Funcionario {
+
+        //Propriedades autoimplementadas
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; private set; }
+
+        //Construtores
+        public Funcionario(int id) {
+            Id = id;
+        }
+        public Funcionario(int id, string name, double salary) : this(id) {
+            Name = name;
+            Salary = salary;
+        }
+
+        //Métodos da classe
+        public void increaseSalary(double percentage) {
+            Salary *= (1 + percentage / 100.0);
+        }
+
+        public override string ToString() {
+            return Id
+                + ", "
+                + Name
+                + ", "
+                + Salary.ToString("F2",CultureInfo.InvariantCulture);
+        }
+
+    }
+}
--- DecimoSegundo/DecimoSegundo/Program.cs
+++ DecimoSegundo/DecimoSegundo/Program.cs
@@ -1,9 +1,49 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
 
 namespace DecimoSegundo {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+
+            List<Funcionario> list = new List<Funcionario>();
+            Console.Write("How many employees will be registered? ");
+            int n = int.Parse(Console.ReadLine());
+
+            //Laço para preencher a lista com objetos do tipo funcionário
+            for (int i = 1; i <= n; i++) {
+                string name;
+                int id;
+                double salary;
+                Console.WriteLine("\nEmployee #" + i + ":");
+                Console.Write("Id: ");
+                id = int.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                name = Console.ReadLine();
+                Console.Write("Salary: ");
+                salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                list.Add(new Funcionario(id, name, salary));
+            }
+
+            //Pesquisa da lista de objetos pelo parâmetro id do objeto.
+            Console.Write("\nEnter the employee id that will have salary increase : ");
+            int idzera = int.Parse(Console.ReadLine());
+            Funcionario pesquisa = list.Find(x => x.Id == idzera);
+            if (pesquisa != null) {
+                Console.Write("Enter the percentage: ");
+                double percentage = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                pesquisa.increaseSalary(percentage);
+
+            }
+            else {
+                Console.WriteLine("This id does not exist!");
+            }
+
+            //updater da lista            
+            Console.WriteLine("\nUpdated list of employees: ");
+            foreach (Funcionario obj in list) {
+                Console.WriteLine(obj);
+            }
         }
     }
 }
